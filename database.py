b056a619643e9b5d52f70088a6814d27b2ce11c1
shubhampandey7860 <109323696+shubhampandey7860@users.noreply.github.com>
2023-08-11 08:31:23 +0000

    backend integration

---
+++ database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, text
+
+# import os
+# db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string ="mysql+pymysql://zwfb1kkhtwgoqjgad0h3:pscale_pw_cXRjttlD7bCoRkxBOoqlQ0RDhdedHyX2UWg0QQA2zLg@aws.connect.psdb.cloud/flaskdb?charset=utf8mb4"
+
+
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("Select *from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._mapping)
+    return jobs
+
+# def load_jobs_from_db_by_id(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(text("Select *from jobs where id  = {id}"))
+#     rows = result.all()
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return dict[rows[0]]
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    if row == None:
+      return "not found",404
+    return row._asdict()
+
+def add_application_to_db(job_id,applications):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, 
+                 job_id=job_id, 
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])
+    
+  
--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify , request 
+from database import load_jobs_from_db , load_job_from_db,add_application_to_db
+from flask_api import status
+
 
 app = Flask(__name__)
-
-Jobs = [{
-  "id": 1,
-  "title": "Data analytics",
-  "location": "Bangalore , India",
-  "salary": "10,00,000"
-}, {
-  "id": 2,
-  "title": "Data Science",
-  "location": "Delhi , India",
-  "salary": "15,00,000"
-}, {
-  "id": 3,
-  "title": "Frontend Engineer",
-  "location": "Remote",
-  "salary": "12,00,000"
-}, {
-  "id": 4,
-  "title": "Backend Engineer",
-  "location": "San Francisco",
-  "salary": "$120,000"
-}]
 
 
 @app.route("/")
 def home():
-  return render_template("home.html", jobs=Jobs)
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs) 
 
 
 @app.route("/api/jobs")
 def listjob():
-  return jsonify(Jobs)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found",status.HTTP_404_NOT_FOUND
+  return render_template("jobtitle.html",job = job)
+@app.route("/contact/")
+def contact():
+  return render_template("contact.html")
+
+
+@app.route("/job/<id>/apply", methods =['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  return render_template('application_submitted.html',application = data,job=job) 
+
 
 
 if __name__ == '__main__':
